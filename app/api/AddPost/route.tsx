a866c2cb1a2f568539dda8860ecb80ea392a5e53
javd boroji <javadboroji22222@gmail.com>
2024-08-09 20:42:41 +0330

    fet:add user login api

--- app/api/AddPost/route.tsx
+++ app/api/AddPost/route.tsx
@@ -1,24 +1,36 @@
 import connectMongoDB from "@/app/Libs/Mongodb";
 import { NextRequest, NextResponse } from "next/server";
-import type { NextApiRequest, NextApiResponse } from 'next';
+import type { NextApiRequest, NextApiResponse } from "next";
 import Posts from "@/app/Moudles/Posts";
 
-export async function POST(req:NextRequest){
-    const data = await req.json();
-    await connectMongoDB();
-    try{
-        let newPost=null
-        if(data){            
-            newPost= await   Posts.create({
-                src:'',
-                date:data.date,
-                author:data.author,
-                title:data.title
-            })
-        }
-        return NextResponse.json({code:200, message:"post sucess added" ,info:newPost});
+export async function POST(req: NextRequest) {
+  const data = await req.formData();
+  await connectMongoDB();
+  try {
+    let newPost = null;
+    if (data) {
+      const imageFile = await data.get("src");
+      if (!imageFile || !(imageFile instanceof File)) {
+        return NextResponse.json({ code: 400, message: "Missing image file" });
+      }
+      const arrayBuffer = await imageFile.arrayBuffer();
+      const buffer = Buffer.from(arrayBuffer);
+      const path = `./file/${imageFile.name}`;
+      await writeFileSync(path, buffer);
+      const imageUrl = imageFile.name || "image.jpg";
+      newPost = await Posts.create({
+        src: imageUrl,
+        date: data.get("date"),
+        author: data.get("author"),
+        title: data.get("title"),
+      });
     }
-    catch(error){
-        return NextResponse.json({code:500, message:error})
-    }
+    return NextResponse.json({
+      code: 200,
+      message: "post sucess added",
+      info: newPost,
+    });
+  } catch (error) {
+    return NextResponse.json({ code: 500, message: error });
+  }
 }
--- app/dashboard/Components/Sidebar.tsx
+++ app/dashboard/Components/Sidebar.tsx
@@ -1,59 +1,72 @@
-import SiteName from '@/app/Components/Header/SitName/SiteName'
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
-import logo from '@/public/blog-logo.png'
+import SiteName from "@/app/Components/Header/SitName/SiteName";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import logo from "@/public/blog-logo.png";
 
 function Sidebar() {
-    const dashboardMenus = [
-        {
-            id: 1,
-            title: ' داشبورد ',
-            url: '/dashboard'
-        },
-        {
-            id: 2,
-            title: ' کانبان ',
-            url: '/dashboard/kanban'
-        },
-        {
-            id: 3,
-            title: 'پست ها ',
-            url: '/dashboard/PostAddOrUpdate'
-        },
-        {
-            id: 4,
-            title: 'کاربران ها ',
-            url: '/dashboard/users'
-        },
-        {
-            id: 5,
-            title: 'کامنت  ها ',
-            url: '/dashboard/comments'
-        },
-        {
-            id: 6,
-            title: 'تنطیمات  ',
-            url: '/dashboard/seting'
-        },
-
-    ]
-    return (
-        <div className='flex flex-col w-[80%] h-[100vh] ' style={{ background: '#14213d' }}>
-            <div className='py-8 flex justify-center'>
-               <Link href={'/'}>
-               <Image alt='logo' src={logo} width={150} height={40} />
-               </Link>
-            </div>
-            <ul className='p-0 my-8'>
-                {dashboardMenus?.map((item, i) => (
-                    <li key={i} className='p-4'>
-                        <Link replace className='text-white text-center ' href={`${item.url}`}>{item.title}</Link>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    )
+  const dashboardMenus = [
+    {
+      id: 11,
+      title: "وب سایت ",
+      url: "/",
+    },
+    {
+      id: 1,
+      title: " داشبورد ",
+      url: "/dashboard",
+    },
+    {   
+      id: 2,
+      title: " کانبان ",
+      url: "/dashboard/kanban",
+    },
+    {
+      id: 3,
+      title: "پست ها ",
+      url: "/dashboard/PostAddOrUpdate",
+    },
+    {
+      id: 4,
+      title: "کاربران ها ",
+      url: "/dashboard/users",
+    },
+    {
+      id: 5,
+      title: "کامنت  ها ",
+      url: "/dashboard/comments",
+    },
+    {
+      id: 6,
+      title: "تنطیمات  ",
+      url: "/dashboard/seting",
+    },
+  ];
+  return (
+    <div
+      className="flex flex-col w-[80%] h-[100vh] "
+      style={{ background: "#14213d" }}
+    >
+      <div className="py-8 flex justify-center">
+        <Link href={"/"}>
+          <Image alt="logo" src={logo} width={150} height={40} />
+        </Link>
+      </div>
+      <ul className="p-0 my-8">
+        {dashboardMenus?.map((item, i) => (
+          <li key={i} className="p-4">
+            <Link
+              replace
+              className="text-white text-center "
+              href={`${item.url}`}
+            >
+              {item.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default Sidebar
+export default Sidebar;
